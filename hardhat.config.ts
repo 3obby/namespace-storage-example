4d27a8dafba87a389e4167353f5b09c2be014954
3obby <rfusseryiii+gh@gmail.com>
2023-08-08 16:03:03 -0500

    updated to latest hgsn

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,5 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "./scripts/hash";
 import "hardhat-generate-storage-namespace";
 
 import { task } from "hardhat/config";
